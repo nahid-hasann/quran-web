7283b8708ca7133e0a580a687bdc9e1ac5176b43
nahid-hasan <mhnahid1w3r@gmail.com>
2026-05-05 19:40:42 +0600

    Connected database to server

--- quran-app/server/index.ts
+++ quran-app/server/index.ts
@@ -2,9 +2,13 @@ import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { Database } from "bun:sqlite";
 import { handle } from 'hono/vercel';
+import path from 'path'; // Eita top-e add koro
 
 const app = new Hono();
 const db = new Database("quran.db");
+
+
+const dbPath = path.resolve(process.cwd(), 'quran.db'); // Eita absolute path nibe
 
 
 
